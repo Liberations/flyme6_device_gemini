49953b36b4d5f84da49a07443c0bec24217e18db
liberations <wenxianglimail@126.com>
2017-05-17 02:17:34 +0800

    InstallBased

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,6 @@
 import common
 import edify_generator
+import os
 
 def ModifyBegin(info):
   edify = info.script
@@ -11,19 +12,39 @@ ui_print("*");
 ui_print("* Romer: Liberation");
 ui_print("******************************************");\n''' + edify.script[0]
 
-  return
+def InstallBased(info):
+    for filename in os.listdir("overlay/firmware-update"):
+        if not (filename.find('.mbn')==-1 and filename.find('.bin')==-1):
+            data=open(os.path.join(os.getcwd(),"overlay/firmware-update",filename)).read()
+            common.ZipWriteStr(info.output_zip, "firmware-update/" + filename, data)
+    extra_img_flash = """ui_print("Patching firmware images");
 
+package_extract_file("firmware-update/cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64");
+package_extract_file("firmware-update/cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib");
+package_extract_file("firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hyp");
+package_extract_file("firmware-update/pmic.elf", "/dev/block/bootdevice/by-name/pmic");
+package_extract_file("firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tz");
+package_extract_file("firmware-update/emmc_appsboot.mbn", "/dev/block/bootdevice/by-name/aboot");
+package_extract_file("firmware-update/devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg");
+package_extract_file("firmware-update/keymaster.mbn", "/dev/block/bootdevice/by-name/keymaster");
+package_extract_file("firmware-update/xbl.elf", "/dev/block/bootdevice/by-name/xbl");
+package_extract_file("firmware-update/rpm.mbn", "/dev/block/bootdevice/by-name/rpm");
 
+package_extract_file("firmware-update/cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64bak");
+package_extract_file("firmware-update/cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlibbak");
+package_extract_file("firmware-update/hyp.mbn", "/dev/block/bootdevice/by-name/hypbak");
+package_extract_file("firmware-update/pmic.elf", "/dev/block/bootdevice/by-name/pmicbak");
+package_extract_file("firmware-update/tz.mbn", "/dev/block/bootdevice/by-name/tzbak");
+package_extract_file("firmware-update/emmc_appsboot.mbn", "/dev/block/bootdevice/by-name/abootbak");
+package_extract_file("firmware-update/devcfg.mbn", "/dev/block/bootdevice/by-name/devcfgbak");
+package_extract_file("firmware-update/keymaster.mbn", "/dev/block/bootdevice/by-name/keymasterbak");
+package_extract_file("firmware-update/rpm.mbn", "/dev/block/bootdevice/by-name/rpmbak");
 
-def AddSuperSU(info):
-    SuperSU = info.input_zip.read("META/SuperSU.zip")
-    common.ZipWriteStr(info.output_zip, "SuperSU/SuperSU.zip", SuperSU)
-    info.script.AppendExtra(('ui_print("{*} Flashing SuperSU");'))
-    info.script.AppendExtra(('package_extract_dir("SuperSU", "/tmp/supersu");'))
-    info.script.AppendExtra(('run_program("/sbin/busybox", "unzip", "/tmp/supersu/SuperSU.zip", "META-INF/com/google/android/*", "-d", "/tmp/supersu");'))
-    info.script.AppendExtra(('run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/supersu/SuperSU.zip");'))
-    info.script.AppendExtra(('ui_print("{*} Finish!");'))
+package_extract_file("firmware-update/NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem");
+package_extract_file("firmware-update/adspso.bin", "/dev/block/bootdevice/by-name/dsp");
+package_extract_file("firmware-update/BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth");"""
+    info.script.AppendExtra(extra_img_flash);
 
 def FullOTA_InstallEnd(info):
     ModifyBegin(info)
-    # AddSuperSU(info)
+    InstallBased(info)
